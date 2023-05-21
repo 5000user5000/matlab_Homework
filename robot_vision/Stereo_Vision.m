26818516f2a41e50e78e8e346f4b138208596eae
Wayne <37620231+5000user5000@users.noreply.github.com>
2023-05-21 22:58:26 +0800

    作業三，多處修正

--- robot_vision/Stereo_Vision.m
+++ robot_vision/Stereo_Vision.m
@@ -37,9 +37,9 @@ Hwc_plus = H1_2*H1;
 
 % Rx(170)
 R2 = [1 0 0 ; 0 cosd(170) -sind(170); 0 sind(170) cosd(170) ];
-H2 = [R1 T2'; 0 0 0 1];
+H2 = [R2 T2'; 0 0 0 1]; %修正，R1改成R2
 % RY(20)
-R2_2 = [0 0 sind(20) ; 0 1 0; -sind(20) 0 cosd(20) ];
+R2_2 = [cosd(20) 0 sind(20) ; 0 1 0; -sind(20) 0 cosd(20) ];%修正
 T2_2 = [0 0 0];
 H2_2 = [R2_2 T2_2'; 0 0 0 1];
 
@@ -47,17 +47,36 @@ H2_2 = [R2_2 T2_2'; 0 0 0 1];
 Hwc_minus = H2_2*H2; 
 
 %%%%% 開始 (b) 小題 %%%%%
-A =[alpha*f 0 u0 ; 0 beta*f v0 ; 0 0 1 ; 0 0 0]; %修正0 0 0
+A =[alpha*f 0 u0 0; 0 beta*f v0 0; 0 0 1 0]; %修正成3*4
 
 
+%驗算是否Hwc+-正確
+% [R T] 留下最後一個row,也就是0 0 0 1 的部分 %修正
+plus_matrix = [Hwc_plus(1:3,:); 0 0 0 1] ;
+minus_matrix =[Hwc_minus(1:3,:); 0 0 0 1] ;
+Pw = zeros(4,8); %世界座標的8個點
+for i = 1:8
+    Pw(:,i) = [Xw(i) Yw(i) Zw(i) 1]; %多一個1是為了和pinv(Hwc_plus)能夠相乘，避免維度不對
+end
+Pc_plus = pinv(Hwc_plus)*Pw; %轉換成相機座標
+Pc_minus = pinv(Hwc_minus)*Pw;
 
-% [R T] 去除最後一個row,也就是0 0 0 1 的部分
-plus_matrix = Hwc_plus(1:3,:);
-minus_matrix = Hwc_minus(1:3,:);
+f_matrix = [f 0 0 0;0 f 0 0; 0 0 1 0];
+image_coord = f_matrix*Pc_plus; % Camera coordinate -> Image plane
+image_coord_minus = f_matrix*Pc_minus;
+for i=1:8
+    image_coord(:,i) = image_coord(:,i)/image_coord(3,i);
+    image_coord_minus(:,i) = image_coord_minus(:,i)/image_coord_minus(3,i);
+end
+
+I2Dconvert_martix = [alpha 0 u0 ; 0 beta v0; 0 0 1]; % Image plane -> Digital image
+digital_plane = I2Dconvert_martix*image_coord;
+digital_plane_minus = I2Dconvert_martix*image_coord_minus;
+
 
 %關係矩陣  A*[R T]
-relation_plus = A*plus_matrix;
-relation_minus = A*minus_matrix;
+relation_plus = A*pinv(Hwc_plus); %P+
+relation_minus = A*pinv(Hwc_minus); %P-
 
 %%%%% 開始 (c) 小題 %%%%%
 % two-view scene reconstruction
@@ -66,7 +85,8 @@ world_points = zeros(3,34);
 
 for i = 1:34
     V =  [skew(Digital_points_plus(:,i))*relation_plus ; skew(Digital_points_minus(:,i))*relation_minus ] ;
-    world_coord = V(:,1:3) \ -V(:,4);
+    %world_coord = V(:,1:3) \ -V(:,4);
+    world_coord = pinv( V(:,1:3) ) * ( -V(:,4) );
     world_points(:,i) = world_coord;
 end
 
@@ -90,4 +110,3 @@ end
 function S = skew(v)
     S = [0 -v(3) v(2); v(3) 0 -v(1); -v(2) v(1) 0];
 end
-
